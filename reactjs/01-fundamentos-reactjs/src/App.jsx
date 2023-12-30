b415ebe51e6eb96a1d93ca12b630f69baa9c8563
emanuelhenrique <emanuel@wcore.com.br>
2023-12-29 21:43:38 -0300

    Init SideBar Style

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import "./global.css";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1> 
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Emanuel Henrique"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati necessitatibus, rem ut dolorem dicta odio. Doloribus aut architecto neque consectetur, voluptas labore dolores expedita nisi? Fuga atque voluptatem unde nulla."
+          />
+        </main>
+      </div>
     </>
   )
 }
